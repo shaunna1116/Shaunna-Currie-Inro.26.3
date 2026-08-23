3e5e2de8719dacd2b2615ac9b10fed941f46714c
Shaunna Currie <leeshaunnaslaughter@gmail.com>
2026-08-22 20:26:26 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -21,3 +21,39 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const userName = event.target.userName.value;
+  const userEmail = event.target.userEmail.value;
+  const userMessage = event.target.userMessage.value;
+
+  console.log(userName, userEmail, userMessage);
+
+  const messageSection = document.getElementById('Messages');
+  const messageList = messageSection.querySelector('ul');
+ 
+
+  const newMessage = document.createElement('li');
+
+  newMessage.innerHTML = `<a href="mailto:${userEmail}">${userName}</a> <span> wrote: ${userMessage}</span>`;
+
+  const removeButton = document.createElement('button');
+
+  removeButton.innerText = 'Remove';
+
+  removeButton.type = 'button';
+
+  removeButton.addEventListener('click', function() {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  event.target.reset();
+});
